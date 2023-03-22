5846d9f095488f9bc936dd0864399140e550a5d9
misha <mikhail.kutovenko@gmail.com>
2023-03-22 14:21:42 +0300

    Add GiftCertificateColumns

---
+++ src/main/java/ru/clevertec/ecl/domain/columns/GiftCertificateColumns.java
@@ -0,0 +1,17 @@
+package ru.clevertec.ecl.domain.columns;
+
+public class GiftCertificateColumns {
+    public static final String ID = "id";
+
+    public static final String NAME = "name";
+
+    public static final String DESCRIPTION = "description";
+
+    public static final String PRICE = "price";
+
+    public static final String DURATION = "duration";
+
+    public static final String CREATE_DATE = "create_date";
+
+    public static final String LAST_UPDATE_DATE = "last_update_date";
+}
