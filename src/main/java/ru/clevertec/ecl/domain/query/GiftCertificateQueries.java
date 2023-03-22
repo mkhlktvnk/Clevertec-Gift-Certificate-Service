76a7b41002e107cd3e716c17504a64405d0932d8
misha <mikhail.kutovenko@gmail.com>
2023-03-22 14:39:56 +0300

    Update GiftCertificateQueries: change access modifiers

--- src/main/java/ru/clevertec/ecl/domain/query/GiftCertificateQueries.java
+++ src/main/java/ru/clevertec/ecl/domain/query/GiftCertificateQueries.java
@@ -1,15 +1,15 @@
 package ru.clevertec.ecl.domain.query;
 
 public class GiftCertificateQueries {
-    private static final String FIND_BY_ID = "SELECT * FROM gift_certificates WHERE id = ?;";
+    public static final String FIND_BY_ID = "SELECT * FROM gift_certificates WHERE id = ?;";
 
-    private static final String FIND_ALL_WITH_LIMIT_AND_OFFSET = "SELECT * FROM gift_certificates LIMIT ? OFFSET ?;";
+    public static final String FIND_ALL_WITH_LIMIT_AND_OFFSET = "SELECT * FROM gift_certificates LIMIT ? OFFSET ?;";
 
-    private static final String INSERT = "INSERT INTO gift_certificates (name, description, price, duration) " +
+    public static final String INSERT = "INSERT INTO gift_certificates (name, description, price, duration) " +
             "VALUES (?, ?, ?, ?);";
 
-    private static final String UPDATE_BY_ID = "UPDATE gift_certificates " +
+    public static final String UPDATE_BY_ID = "UPDATE gift_certificates " +
             "SET name = ?, description = ?, price = ?, duration = ? WHERE id = ?;";
 
-    private static final String DELETE_BY_ID = "DELETE FROM gift_certificates WHERE id = ?;";
+    public static final String DELETE_BY_ID = "DELETE FROM gift_certificates WHERE id = ?;";
 }
