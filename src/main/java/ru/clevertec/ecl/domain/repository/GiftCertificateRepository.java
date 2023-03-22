ab6809132e2e58e5ab20dbae642fb00676bb2383
misha <mikhail.kutovenko@gmail.com>
2023-03-22 15:23:19 +0300

    Add GiftCertificateRepository

---
+++ src/main/java/ru/clevertec/ecl/domain/repository/GiftCertificateRepository.java
@@ -0,0 +1,14 @@
+package ru.clevertec.ecl.domain.repository;
+
+import ru.clevertec.ecl.domain.entity.GiftCertificate;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GiftCertificateRepository {
+    List<GiftCertificate> findAll(int page, int size);
+    Optional<GiftCertificate> findById(Long id);
+    GiftCertificate insert(GiftCertificate giftCertificate);
+    void update(Long id, GiftCertificate giftCertificate);
+    void delete(Long id);
+}
