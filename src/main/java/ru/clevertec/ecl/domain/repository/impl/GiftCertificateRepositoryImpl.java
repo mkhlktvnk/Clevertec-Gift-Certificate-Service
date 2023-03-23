df7fdf7cf6514cb8c2169e66c21ab581beb6cdb8
misha <mikhail.kutovenko@gmail.com>
2023-03-23 08:38:51 +0300

    Update GiftCertificateRepositoryImpl: refactor imports

--- src/main/java/ru/clevertec/ecl/domain/repository/impl/GiftCertificateRepositoryImpl.java
+++ src/main/java/ru/clevertec/ecl/domain/repository/impl/GiftCertificateRepositoryImpl.java
@@ -18,7 +18,6 @@ import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
