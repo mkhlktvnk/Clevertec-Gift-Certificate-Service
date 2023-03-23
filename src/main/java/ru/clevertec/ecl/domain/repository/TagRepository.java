c3395967f5656418f1d8921ba0f5c5859b2f7eea
misha <mikhail.kutovenko@gmail.com>
2023-03-23 09:29:49 +0300

    Add TagRepository

---
+++ src/main/java/ru/clevertec/ecl/domain/repository/TagRepository.java
@@ -0,0 +1,13 @@
+package ru.clevertec.ecl.domain.repository;
+
+import ru.clevertec.ecl.domain.entity.Tag;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TagRepository {
+    List<Tag> findAll(int page, int size);
+    Optional<Tag> findById(long id);
+    Tag insert(Tag tag);
+    void delete(Long id);
+}
