16f790ee0e8eba96cbed253488d3055fc161df16
misha <mikhail.kutovenko@gmail.com>
2023-03-23 19:45:29 +0300

    Add Tables

---
+++ src/main/java/ru/clevertec/ecl/domain/constants/table/Tables.java
@@ -0,0 +1,9 @@
+package ru.clevertec.ecl.domain.constants.table;
+
+public class Tables {
+    public static final String GIFT_CERTIFICATES_TABLE = "gift_certificates";
+
+    public static final String TAGS_TABLE = "tags";
+
+    public static final String GIFT_CERTIFICATES_TAGS_TABLE = "gift_certificates_tags";
+}
