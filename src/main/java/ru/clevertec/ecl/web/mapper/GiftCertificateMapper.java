b45288bdc438fb79dd4a6e3ac948bcf516b4dfb6
misha <mikhail.kutovenko@gmail.com>
2023-03-24 22:44:46 +0300

    Add GiftCertificateMapper

---
+++ src/main/java/ru/clevertec/ecl/web/mapper/GiftCertificateMapper.java
@@ -0,0 +1,15 @@
+package ru.clevertec.ecl.web.mapper;
+
+import org.mapstruct.Mapper;
+import ru.clevertec.ecl.domain.entity.GiftCertificate;
+import ru.clevertec.ecl.web.model.GiftCertificateModel;
+
+import java.util.Collection;
+import java.util.List;
+
+@Mapper
+public interface GiftCertificateMapper {
+    GiftCertificate mapToEntity(GiftCertificateModel model);
+    GiftCertificateModel mapToModel(GiftCertificate entity);
+    List<GiftCertificateModel> mapToModel(Collection<GiftCertificate> entities);
+}
