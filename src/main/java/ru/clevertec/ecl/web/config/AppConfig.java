08fda3ae6584b461acd6754d89d77f4424d6e304
misha <mikhail.kutovenko@gmail.com>
2023-03-24 19:42:19 +0300

    Add AppConfig

---
+++ src/main/java/ru/clevertec/ecl/web/config/AppConfig.java
@@ -0,0 +1,12 @@
+package ru.clevertec.ecl.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ru.clevertec.ecl")
+public class AppConfig {
+
+}
