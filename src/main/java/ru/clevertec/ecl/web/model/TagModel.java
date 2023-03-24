cb8d32090f60b5f76eac6d29876e4cd4c8fe8ec3
misha <mikhail.kutovenko@gmail.com>
2023-03-24 20:33:50 +0300

    Add TagModel

---
+++ src/main/java/ru/clevertec/ecl/web/model/TagModel.java
@@ -0,0 +1,23 @@
+package ru.clevertec.ecl.web.model;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagModel {
+    @NotNull
+    @Min(0)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 255)
+    private String name;
+}
