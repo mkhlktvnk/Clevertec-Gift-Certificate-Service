f64e7d43eabd962ab9acf20d8aeded81cc2c88b1
misha <mikhail.kutovenko@gmail.com>
2023-03-24 17:18:46 +0300

    Add GiftCertificateService

---
+++ src/main/java/ru/clevertec/ecl/service/GiftCertificateService.java
@@ -0,0 +1,10 @@
+package ru.clevertec.ecl.service;
+
+import ru.clevertec.ecl.domain.entity.GiftCertificate;
+
+public interface GiftCertificateService {
+    GiftCertificate getById(long id);
+    GiftCertificate save(GiftCertificate giftCertificate);
+    void updateById(long id, GiftCertificate giftCertificate);
+    void deleteById(long id);
+}
