2230e5a9675c63ecb9277d54d472584c6e425536
misha <mikhail.kutovenko@gmail.com>
2023-03-24 19:38:14 +0300

    Update TagQueries: add update query

--- src/main/java/ru/clevertec/ecl/domain/query/TagQueries.java
+++ src/main/java/ru/clevertec/ecl/domain/query/TagQueries.java
@@ -7,6 +7,8 @@ public class TagQueries {
 
     public static final String INSERT = "INSERT INTO tags (name) VALUES ?;";
 
+    public static final String UPDATE = "UPDATE tags SET name = ? WHERE id = ?;";
+
     public static final String DELETE_BY_ID = "DELETE FROM tags WHERE ID = ?;";
 
     public static final String SELECT_COUNT_BY_ID = "SELECT COUNT(*) FROM tags WHERE id = ?;";
