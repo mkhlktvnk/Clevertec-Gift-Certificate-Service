f878162788b75cae793dae5965ec08f77c22c451
misha <mikhail.kutovenko@gmail.com>
2023-03-24 17:23:37 +0300

    Add TagService

---
+++ src/main/java/ru/clevertec/ecl/service/TagService.java
@@ -0,0 +1,10 @@
+package ru.clevertec.ecl.service;
+
+import ru.clevertec.ecl.domain.entity.Tag;
+
+public interface TagService {
+    Tag getById(long id);
+    Tag insert(Tag tag);
+    void updateById(long id, Tag tag);
+    void deleteById(long id);
+}
