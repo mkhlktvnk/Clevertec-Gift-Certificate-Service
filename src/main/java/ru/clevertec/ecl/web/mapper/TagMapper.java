26f3a59467185314b7608eb756d284fc106e26d4
misha <mikhail.kutovenko@gmail.com>
2023-03-24 20:37:33 +0300

    Update TagMapper: add method to map collection of tags

--- src/main/java/ru/clevertec/ecl/web/mapper/TagMapper.java
+++ src/main/java/ru/clevertec/ecl/web/mapper/TagMapper.java
@@ -4,8 +4,12 @@ import org.mapstruct.Mapper;
 import ru.clevertec.ecl.domain.entity.Tag;
 import ru.clevertec.ecl.web.model.TagModel;
 
+import java.util.Collection;
+import java.util.List;
+
 @Mapper
 public interface TagMapper {
     Tag mapToEntity(TagModel model);
     TagModel mapToModel(Tag entity);
+    List<TagModel> mapToModel(Collection<Tag> entities);
 }
