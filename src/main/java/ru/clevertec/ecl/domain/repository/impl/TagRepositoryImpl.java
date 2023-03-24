357e3ab29451f962f8283098ed711703c996d936
misha <mikhail.kutovenko@gmail.com>
2023-03-24 16:23:27 +0300

    Update TagRepositoryImpl: change imports

--- src/main/java/ru/clevertec/ecl/domain/repository/impl/TagRepositoryImpl.java
+++ src/main/java/ru/clevertec/ecl/domain/repository/impl/TagRepositoryImpl.java
@@ -1,11 +1,15 @@
 package ru.clevertec.ecl.domain.repository.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.clevertec.ecl.domain.entity.Tag;
+import ru.clevertec.ecl.domain.query.GiftCertificateQueries;
+import ru.clevertec.ecl.domain.query.TagQueries;
 import ru.clevertec.ecl.domain.repository.TagRepository;
+import ru.clevertec.ecl.domain.repository.exception.DomainException;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,12 +22,24 @@ public class TagRepositoryImpl implements TagRepository {
 
     @Override
     public List<Tag> findAll(int page, int size) {
-        return null;
+        List<Tag> tags;
+        try {
+            tags = jdbcTemplate.query(TagQueries.FIND_WITH_LIMIT_AND_OFFSET,
+                    new Object[]{ size, page }, mapper);
+        } catch (DataAccessException e) {
+            throw new DomainException(e.getMessage(), e);
+        }
+        return tags;
     }
 
     @Override
     public Optional<Tag> findById(long id) {
-        return Optional.empty();
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(TagQueries.FIND_BY_ID,
+                    new Object[]{id}, mapper));
+        } catch (DataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
