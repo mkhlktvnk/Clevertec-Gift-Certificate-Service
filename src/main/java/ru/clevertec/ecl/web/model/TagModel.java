1281e59bd6ffdfceb1a4c3081ea8d55e5a3b17ee
misha <mikhail.kutovenko@gmail.com>
2023-03-26 18:03:23 +0300

    Update TagModel: add @Getter, @Setter and @NoArgsConstructor

--- src/main/java/ru/clevertec/ecl/web/model/TagModel.java
+++ src/main/java/ru/clevertec/ecl/web/model/TagModel.java
@@ -7,8 +7,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
-@Builder
 @Getter
+@Setter
+@NoArgsConstructor
 public class TagModel {
     @NotNull
     @Min(0)
