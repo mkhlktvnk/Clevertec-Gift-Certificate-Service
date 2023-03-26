3f13c145ce5d6bb5d7d4befdf1c034b182f5c2c2
misha <mikhail.kutovenko@gmail.com>
2023-03-26 16:45:46 +0300

    Update GiftCertificateRepository: change parameters of findAll

--- src/main/java/ru/clevertec/ecl/domain/repository/GiftCertificateRepository.java
+++ src/main/java/ru/clevertec/ecl/domain/repository/GiftCertificateRepository.java
@@ -1,12 +1,14 @@
 package ru.clevertec.ecl.domain.repository;
 
+import org.springframework.data.domain.Pageable;
 import ru.clevertec.ecl.domain.entity.GiftCertificate;
+import ru.clevertec.ecl.web.criteria.GiftCertificateCriteria;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface GiftCertificateRepository {
-    List<GiftCertificate> findAll(int page, int size);
+    List<GiftCertificate> findAll(Pageable pageable, GiftCertificateCriteria criteria);
     Optional<GiftCertificate> findById(Long id);
     GiftCertificate insert(GiftCertificate giftCertificate);
     void update(Long id, GiftCertificate giftCertificate);
