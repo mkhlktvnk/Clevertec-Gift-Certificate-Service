e62d96b715aff635c25678d81b4fa6b6d4404b6b
misha <mikhail.kutovenko@gmail.com>
2023-03-26 23:33:59 +0300

    Add ApplicationProdProfile

---
+++ src/main/java/ru/clevertec/ecl/config/profile/ApplicationProdProfile.java
@@ -0,0 +1,11 @@
+package ru.clevertec.ecl.config.profile;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Profile("prod")
+@PropertySource("classpath:application-dev.properties")
+public class ApplicationProdProfile {
+}
