9d1901bd050a94d53ab4339db7713d968094f9c6
misha <mikhail.kutovenko@gmail.com>
2023-03-26 23:33:48 +0300

    Add ApplicationDevProfile

---
+++ src/main/java/ru/clevertec/ecl/config/profile/ApplicationDevProfile.java
@@ -0,0 +1,11 @@
+package ru.clevertec.ecl.config.profile;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Profile("dev")
+@PropertySource("classpath:application-dev.properties")
+public class ApplicationDevProfile {
+}
