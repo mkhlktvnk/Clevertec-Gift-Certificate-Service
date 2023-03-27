12504fb314b05694a6f08814a50cd3e7b522fb3f
misha <mikhail.kutovenko@gmail.com>
2023-03-27 22:16:19 +0300

    Update TagRepositoryTest: refactor imports

--- src/test/java/ru/clevertec/ecl/domain/repository/TagRepositoryTest.java
+++ src/test/java/ru/clevertec/ecl/domain/repository/TagRepositoryTest.java
@@ -8,7 +8,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.transaction.annotation.Transactional;
 import ru.clevertec.ecl.domain.entity.Tag;
 import ru.clevertec.ecl.domain.mapper.TagMapper;
 import ru.clevertec.ecl.domain.repository.exception.DomainException;
