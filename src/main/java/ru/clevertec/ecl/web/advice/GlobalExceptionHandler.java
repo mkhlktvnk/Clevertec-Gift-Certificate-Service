1f978129135de887d1ca44abf580886a16bd9a0a
misha <mikhail.kutovenko@gmail.com>
2023-03-28 00:24:19 +0300

    Update GlobalExceptionHandler

--- src/main/java/ru/clevertec/ecl/web/advice/GlobalExceptionHandler.java
+++ src/main/java/ru/clevertec/ecl/web/advice/GlobalExceptionHandler.java
@@ -1,19 +1,20 @@
 package ru.clevertec.ecl.web.advice;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.clevertec.ecl.service.exception.ResourceNotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ApiError handleResourceNotFoundException(ResourceNotFoundException e) {
-        return ApiError.builder()
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException e) {
+        ApiError apiError = ApiError.builder()
                 .message(e.getMessage())
-                .statusCode(HttpStatus.NOT_FOUND.value())
+                .statusCode(HttpStatus.NO_CONTENT.value())
                 .build();
+
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
     }
 }
