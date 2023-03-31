8fad026b7e170ecb56db4eb46831e3dbcec82ec1
misha <mikhail.kutovenko@gmail.com>
2023-04-01 01:30:54 +0300

    Update TagRepositoryImpl: remove unnecessary methods and migrate to hibernate

--- src/main/java/ru/clevertec/ecl/domain/repository/impl/TagRepositoryImpl.java
+++ src/main/java/ru/clevertec/ecl/domain/repository/impl/TagRepositoryImpl.java
@@ -1,25 +1,20 @@
 package ru.clevertec.ecl.domain.repository.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import ru.clevertec.ecl.domain.constant.column.TagColumns;
 import ru.clevertec.ecl.domain.entity.Tag;
 import ru.clevertec.ecl.domain.repository.TagRepository;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class TagRepositoryImpl implements TagRepository {
     private final SessionFactory sessionFactory;
-    private final JdbcTemplate jdbcTemplate;
-    private final RowMapper<Tag> mapper;
 
     @Override
     public List<Tag> findAll(Pageable pageable) {
@@ -27,42 +22,49 @@ public class TagRepositoryImpl implements TagRepository {
     }
 
     @Override
-    public List<Tag> findByGiftCertificateId(long id) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
     public Optional<Tag> findById(long id) {
-        throw new UnsupportedOperationException();
+        try (Session session = sessionFactory.openSession()) {
+            return Optional.ofNullable(session.get(Tag.class, id));
+        }
     }
 
     @Override
     public Tag insert(Tag tag) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Tag insertAndAddToGiftCertificate(long tagCertificateId, Tag tag) {
-        throw new UnsupportedOperationException();
+        try (Session session = sessionFactory.openSession()) {
+            session.getTransaction().begin();
+            session.persist(tag);
+            session.flush();
+            session.getTransaction().commit();
+            return tag;
+        }
     }
 
     @Override
     public void update(Long id, Tag updateTag) {
+        try (Session session = sessionFactory.openSession()) {
+            session.getTransaction().begin();
+            Tag tag = session.get(Tag.class, id);
+            tag.setName(updateTag.getName());
+            session.flush();
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void delete(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            session.getTransaction().begin();
+            Tag tag = session.get(Tag.class, id);
+            session.remove(tag);
+            session.flush();
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public boolean existsById(Long id) {
-        throw new UnsupportedOperationException();
-    }
-
-    private Tag mapInsertResult(Map<String, Object> map) {
-        return Tag.builder()
-                .id((Long) map.get(TagColumns.ID))
-                .name((String) map.get(TagColumns.NAME))
-                .build();
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Tag.class, id) != null;
+        }
     }
 }
