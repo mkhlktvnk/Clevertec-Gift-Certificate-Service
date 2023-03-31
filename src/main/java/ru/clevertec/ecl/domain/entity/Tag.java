6e4a4bd4119a5e7437a20b78533a683c46e5146c
misha <mikhail.kutovenko@gmail.com>
2023-04-01 00:21:57 +0300

    Update entites

--- src/main/java/ru/clevertec/ecl/domain/entity/Tag.java
+++ src/main/java/ru/clevertec/ecl/domain/entity/Tag.java
@@ -1,5 +1,12 @@
 package ru.clevertec.ecl.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -7,6 +14,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import ru.clevertec.ecl.domain.repository.impl.Identifiable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,7 +26,17 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
-public class Tag {
+@Entity
+@Table(name = "tags")
+public class Tag implements Identifiable<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<GiftCertificate> giftCertificates = new ArrayList<>();
 }
--- src/main/java/ru/clevertec/ecl/domain/repository/impl/TagRepositoryImpl.java
+++ src/main/java/ru/clevertec/ecl/domain/repository/impl/TagRepositoryImpl.java
@@ -1,18 +1,14 @@
 package ru.clevertec.ecl.domain.repository.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataAccessException;
+import org.hibernate.SessionFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.clevertec.ecl.domain.constant.column.TagColumns;
-import ru.clevertec.ecl.domain.constant.table.Tables;
 import ru.clevertec.ecl.domain.entity.Tag;
-import ru.clevertec.ecl.domain.query.TagQueries;
 import ru.clevertec.ecl.domain.repository.TagRepository;
-import ru.clevertec.ecl.domain.repository.exception.DomainException;
 
 import java.util.List;
 import java.util.Map;
@@ -21,91 +17,46 @@ import java.util.Optional;
 @Repository
 @RequiredArgsConstructor
 public class TagRepositoryImpl implements TagRepository {
+    private final SessionFactory sessionFactory;
     private final JdbcTemplate jdbcTemplate;
     private final RowMapper<Tag> mapper;
 
     @Override
     public List<Tag> findAll(Pageable pageable) {
-        try {
-            return jdbcTemplate.query(TagQueries.FIND_WITH_LIMIT_AND_OFFSET, mapper,
-                    pageable.getPageSize(), pageable.getOffset());
-        } catch (DataAccessException e) {
-            throw new DomainException(e.getMessage(), e);
-        }
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public List<Tag> findByGiftCertificateId(long id) {
-        try {
-            return jdbcTemplate.query(TagQueries.FIND_TAGS_BY_GIFT_CERTIFICATE_ID, mapper, id);
-        } catch (DataAccessException e) {
-            throw new DomainException(e.getMessage(), e);
-        }
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Optional<Tag> findById(long id) {
-        try {
-            return Optional.ofNullable(jdbcTemplate.queryForObject(TagQueries.FIND_BY_ID, mapper, id));
-        } catch (DataAccessException e) {
-            return Optional.empty();
-        }
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Tag insert(Tag tag) {
-        try {
-            long id = new SimpleJdbcInsert(jdbcTemplate)
-                    .withTableName(Tables.TAGS_TABLE)
-                    .usingColumns(TagColumns.NAME)
-                    .usingGeneratedKeyColumns(TagColumns.ID)
-                    .executeAndReturnKey(Map.of(TagColumns.NAME, tag.getName()))
-                    .longValue();
-            return findById(id).orElseThrow(DomainException::new);
-        } catch (DataAccessException e) {
-            throw new DomainException(e.getMessage(), e);
-        }
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Tag insertAndAddToGiftCertificate(long tagCertificateId, Tag tag) {
-        try {
-            Tag inserted = insert(tag);
-            jdbcTemplate.update(TagQueries.ADD_TAG_TO_GIFT_CERTIFICATE,
-                    tagCertificateId, inserted.getId());
-            return inserted;
-        } catch (DataAccessException e) {
-            throw new DomainException(e.getMessage(), e);
-        }
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void update(Long id, Tag updateTag) {
-        try {
-            jdbcTemplate.update(TagQueries.UPDATE, updateTag.getName(), id);
-        } catch (DataAccessException e) {
-            throw new DomainException(e.getMessage(), e);
-        }
     }
 
     @Override
     public void delete(Long id) {
-        try {
-            jdbcTemplate.update(TagQueries.DELETE_BY_ID, id);
-        } catch (DataAccessException e) {
-            throw new DomainException(e.getMessage(), e);
-        }
     }
 
     @Override
     public boolean existsById(Long id) {
-        try {
-            Integer result = jdbcTemplate.queryForObject(TagQueries.SELECT_COUNT_BY_ID,
-                    Integer.class, id);
-            return result > 0;
-        } catch (DataAccessException e) {
-            throw new DomainException(e.getMessage(), e);
-        }
+        throw new UnsupportedOperationException();
     }
 
     private Tag mapInsertResult(Map<String, Object> map) {
