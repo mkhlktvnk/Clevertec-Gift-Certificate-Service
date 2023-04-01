4c24a471a9067ceeb5017b4e23700163c5f2c661
misha <mikhail.kutovenko@gmail.com>
2023-04-01 04:02:30 +0300

    Remove unnecessary classes

--- src/main/java/ru/clevertec/ecl/domain/constant/table/Tables.java
+++
@@ -1,9 +0,0 @@
-package ru.clevertec.ecl.domain.constant.table;
-
-public class Tables {
-    public static final String GIFT_CERTIFICATES_TABLE = "gift_certificates";
-
-    public static final String TAGS_TABLE = "tags";
-
-    public static final String GIFT_CERTIFICATES_TAGS_TABLE = "gift_certificates_tags";
-}
