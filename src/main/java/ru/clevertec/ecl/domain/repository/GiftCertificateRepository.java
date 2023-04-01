78d45a8edf289329bce72684bcdf151caf100045
misha <mikhail.kutovenko@gmail.com>
2023-04-01 03:04:06 +0300

    Update GiftCertificateRepository: change findAll method

--- src/main/java/ru/clevertec/ecl/domain/repository/GiftCertificateRepository.java
+++ src/main/java/ru/clevertec/ecl/domain/repository/GiftCertificateRepository.java
@@ -1,6 +1,7 @@
 package ru.clevertec.ecl.domain.repository;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import ru.clevertec.ecl.domain.entity.GiftCertificate;
 import ru.clevertec.ecl.web.criteria.GiftCertificateCriteria;
 
@@ -8,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface GiftCertificateRepository {
-    List<GiftCertificate> findAll(Pageable pageable, GiftCertificateCriteria criteria);
+    List<GiftCertificate> findAll(Pageable pageable, Specification<GiftCertificate> specification);
 
     Optional<GiftCertificate> findById(Long id);
 
