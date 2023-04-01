c5997f744ff0e92465e6f88238574fcd32e0acb8
misha <mikhail.kutovenko@gmail.com>
2023-04-01 03:56:18 +0300

    Update entities: refactor imports

--- src/main/java/ru/clevertec/ecl/domain/entity/GiftCertificate.java
+++ src/main/java/ru/clevertec/ecl/domain/entity/GiftCertificate.java
@@ -11,13 +11,10 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
