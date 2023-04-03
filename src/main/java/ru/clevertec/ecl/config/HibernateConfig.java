6544096c0805d11a97474cc508f40671fd90f5c0
misha <mikhail.kutovenko@gmail.com>
2023-04-04 01:48:23 +0300

    Refactor imports

--- src/main/java/ru/clevertec/ecl/config/HibernateConfig.java
+++ src/main/java/ru/clevertec/ecl/config/HibernateConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
