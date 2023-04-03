4dec64fba2664405a463ce154c6976bc7207fdec
misha <mikhail.kutovenko@gmail.com>
2023-04-04 02:26:32 +0300

    Update HibernateConfig

--- src/main/java/ru/clevertec/ecl/config/HibernateConfig.java
+++ src/main/java/ru/clevertec/ecl/config/HibernateConfig.java
@@ -5,6 +5,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
@@ -16,6 +17,7 @@ import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
+@Profile("dev")
 @PropertySource("classpath:application.properties")
 public class HibernateConfig {
 
