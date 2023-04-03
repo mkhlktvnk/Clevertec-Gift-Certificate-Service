f116cee1fdb662bdcab191026e8f7f46faf3e415
misha <mikhail.kutovenko@gmail.com>
2023-04-03 23:09:16 +0300

    Update GiftCertificateRepositoryImpl & GiftCertificateServiceImpl: refactor

--- src/main/java/ru/clevertec/ecl/domain/repository/impl/GiftCertificateRepositoryImpl.java
+++ src/main/java/ru/clevertec/ecl/domain/repository/impl/GiftCertificateRepositoryImpl.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.query.QueryUtils;
 import org.springframework.stereotype.Repository;
 import ru.clevertec.ecl.domain.entity.GiftCertificate;
+import ru.clevertec.ecl.domain.entity.Tag;
 import ru.clevertec.ecl.domain.repository.GiftCertificateRepository;
 
 import java.util.List;
@@ -23,72 +24,75 @@ public class GiftCertificateRepositoryImpl implements GiftCertificateRepository 
 
     @Override
     public List<GiftCertificate> findAll(Pageable pageable, Specification<GiftCertificate> specification) {
-        try (Session session = sessionFactory.openSession()) {
-            CriteriaBuilder builder = session.getCriteriaBuilder();
-            CriteriaQuery<GiftCertificate> query =
-                    builder.createQuery(GiftCertificate.class);
-            Root<GiftCertificate> root =
-                    query.from(GiftCertificate.class);
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
 
-            query.select(root)
-                    .where(specification.toPredicate(root, query, builder))
-                    .orderBy(QueryUtils.toOrders(pageable.getSort(), root, builder));
+        CriteriaQuery<GiftCertificate> query =
+                builder.createQuery(GiftCertificate.class);
+        Root<GiftCertificate> root =
+                query.from(GiftCertificate.class);
 
-            return session.createQuery(query)
-                    .setFirstResult(pageable.getPageNumber())
-                    .setMaxResults(pageable.getPageSize())
-                    .getResultList();
-        }
+        query.select(root)
+                .where(specification.toPredicate(root, query, builder))
+                .orderBy(QueryUtils.toOrders(pageable.getSort(), root, builder));
+
+        return session.createQuery(query)
+                .setFirstResult(pageable.getPageNumber())
+                .setMaxResults(pageable.getPageSize())
+                .getResultList();
     }
 
     @Override
     public Optional<GiftCertificate> findById(Long id) {
-        try (Session session = sessionFactory.openSession()) {
-            return Optional.ofNullable(session.get(GiftCertificate.class, id));
-        }
+        Session session = sessionFactory.getCurrentSession();
+        return Optional.ofNullable(session.get(GiftCertificate.class, id));
     }
 
     @Override
     public GiftCertificate insert(GiftCertificate giftCertificate) {
-        try (Session session = sessionFactory.openSession()) {
-            session.getTransaction().begin();
-            session.persist(giftCertificate);
-            session.flush();
-            session.getTransaction().commit();
-            return giftCertificate;
+        Session session = sessionFactory.getCurrentSession();
+        if (giftCertificate.getTags() != null) {
+            List<Tag> mergedTags = mergeTags(giftCertificate.getTags(), session);
+            giftCertificate.setTags(mergedTags);
         }
+        session.persist(giftCertificate);
+        session.flush();
+        return giftCertificate;
     }
 
     @Override
     public void update(Long id, GiftCertificate updateCertificate) {
-        try (Session session = sessionFactory.openSession()) {
-            session.getTransaction().begin();
-            GiftCertificate certificate = session.get(GiftCertificate.class, id);
-            certificate.setName(updateCertificate.getName());
-            certificate.setDescription(updateCertificate.getDescription());
-            certificate.setPrice(updateCertificate.getPrice());
-            certificate.setDuration(updateCertificate.getDuration());
-            certificate.getTags().addAll(updateCertificate.getTags());
-            session.flush();
-            session.getTransaction().commit();
+        Session session = sessionFactory.getCurrentSession();
+        GiftCertificate certificate = session.get(GiftCertificate.class, id);
+
+        certificate.setName(updateCertificate.getName());
+        certificate.setDescription(updateCertificate.getDescription());
+        certificate.setPrice(updateCertificate.getPrice());
+        certificate.setDuration(updateCertificate.getDuration());
+
+        if (updateCertificate.getTags() != null) {
+            List<Tag> mergedTags = mergeTags(updateCertificate.getTags(), session);
+            certificate.getTags().addAll(mergedTags);
         }
+        session.flush();
     }
 
     @Override
     public void delete(Long id) {
-        try (Session session = sessionFactory.openSession()) {
-            session.getTransaction().begin();
-            GiftCertificate certificate = session.get(GiftCertificate.class, id);
-            session.remove(certificate);
-            session.flush();
-            session.getTransaction().commit();
-        }
+        Session session = sessionFactory.getCurrentSession();
+        GiftCertificate certificate =
+                session.get(GiftCertificate.class, id);
+        session.remove(certificate);
+        session.flush();
     }
 
     @Override
     public boolean existsById(Long id) {
-        try (Session session = sessionFactory.openSession()) {
-            return session.get(GiftCertificate.class, id) != null;
-        }
+        return sessionFactory.getCurrentSession()
+                .get(GiftCertificate.class, id) != null;
+    }
+
+    private List<Tag> mergeTags(List<Tag> detachedTags, Session session) {
+        return detachedTags.stream().map(session::merge).toList();
     }
 }
