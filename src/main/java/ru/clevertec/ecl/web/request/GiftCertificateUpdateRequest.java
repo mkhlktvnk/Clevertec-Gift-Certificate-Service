f9901642dedd6b291a13ae30163cf5b4149e1e71
misha <mikhail.kutovenko@gmail.com>
2023-04-07 20:26:20 +0300

    Refactor imports

--- src/main/java/ru/clevertec/ecl/web/request/GiftCertificateUpdateRequest.java
+++ src/main/java/ru/clevertec/ecl/web/request/GiftCertificateUpdateRequest.java
@@ -3,7 +3,11 @@ package ru.clevertec.ecl.web.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
