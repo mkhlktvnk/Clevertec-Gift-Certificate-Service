f809affbbbeb252b05fe82a0ed73f6e6c0c4a6e1
misha <mikhail.kutovenko@gmail.com>
2023-04-07 11:23:43 +0300

    Update OrderRepository: add method to find orders by user id

--- src/main/java/ru/clevertec/ecl/domain/repository/OrderRepository.java
+++ src/main/java/ru/clevertec/ecl/domain/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package ru.clevertec.ecl.domain.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.clevertec.ecl.domain.entity.Order;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByUserId(long userId, Pageable pageable);
 }
