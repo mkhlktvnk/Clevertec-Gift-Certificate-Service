fdb195dd1f5ce5c37b13c2efcb9cb69924256b8c
misha <mikhail.kutovenko@gmail.com>
2023-04-07 10:02:10 +0300

    Add @Repository annotation

--- src/main/java/ru/clevertec/ecl/domain/repository/TagRepository.java
+++ src/main/java/ru/clevertec/ecl/domain/repository/TagRepository.java
@@ -1,8 +1,10 @@
 package ru.clevertec.ecl.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.clevertec.ecl.domain.entity.Tag;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
 }
