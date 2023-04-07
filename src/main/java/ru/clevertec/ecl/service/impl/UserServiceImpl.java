ba0252d7d5f6835d0ff663d7e8a409fa65d527ae
misha <mikhail.kutovenko@gmail.com>
2023-04-07 10:01:14 +0300

    Add UserService

---
+++ src/main/java/ru/clevertec/ecl/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package ru.clevertec.ecl.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import ru.clevertec.ecl.domain.entity.User;
+import ru.clevertec.ecl.domain.repository.UserRepository;
+import ru.clevertec.ecl.service.UserService;
+import ru.clevertec.ecl.service.exception.ResourceNotFoundException;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public List<User> findAllByPageable(Pageable pageable) {
+        return userRepository.findAll(pageable).getContent();
+    }
+
+    @Override
+    public User findById(long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found!"));
+    }
+
+}
