239e28a6c8cc296a3476fb90d9e8156e6435d446
misha <mikhail.kutovenko@gmail.com>
2023-04-10 19:05:23 +0300

    Fix GiftCertificateCriteria

--- src/main/java/ru/clevertec/ecl/web/criteria/GiftCertificateCriteria.java
+++ src/main/java/ru/clevertec/ecl/web/criteria/GiftCertificateCriteria.java
@@ -1,22 +1,23 @@
 package ru.clevertec.ecl.web.criteria;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class GiftCertificateCriteria {
-    @NotBlank
+
     @Size(min = 1)
     private String name;
 
-    @NotBlank
     @Size(min = 1)
     private String description;
 
     private List<String> tagName;
+
 }
