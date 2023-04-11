61503193d7b02adb4808061f0c0fc8cc6b512e3a
misha <mikhail.kutovenko@gmail.com>
2023-04-11 14:28:01 +0300

    Update schema.sql

--- src/main/resources/sql/scripts/dev/schema.sql
+++ src/main/resources/sql/scripts/dev/schema.sql
@@ -1,45 +1,44 @@
-CREATE OR REPLACE FUNCTION trigger_set_last_update_date()
-    RETURNS TRIGGER AS
-$$
-BEGIN
-    NEW.last_update_date = NOW();
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+CREATE TABLE IF NOT EXISTS users
+(
+    id       SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email    VARCHAR(255) NOT NULL UNIQUE
+);
 
 CREATE TABLE IF NOT EXISTS tags
 (
-    id   BIGSERIAL    NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS orders
+(
+    id                  SERIAL PRIMARY KEY,
+    total_price         NUMERIC(19, 2) NOT NULL,
+    purchase_time       TIMESTAMP      NOT NULL,
+    gift_certificate_id BIGINT         NOT NULL,
+    user_id             BIGINT         NOT NULL,
+    CONSTRAINT fk_orders_gift_certificate_id FOREIGN KEY (gift_certificate_id) REFERENCES gift_certificates (id),
+    CONSTRAINT fk_orders_user_id FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE IF NOT EXISTS gift_certificates
 (
-    id               BIGSERIAL      NOT NULL,
+    id               SERIAL PRIMARY KEY,
     name             VARCHAR(255)   NOT NULL,
-    description      VARCHAR(1000)  NOT NULL,
-    price            NUMERIC(40, 2) NOT NULL,
-    duration         INT            NOT NULL,
-    create_date      TIMESTAMP      DEFAULT NOW(),
-    last_update_date TIMESTAMP      DEFAULT NOW(),
-    PRIMARY KEY (id)
+    description      TEXT           NOT NULL,
+    price            NUMERIC(19, 2) NOT NULL,
+    duration         INTEGER        NOT NULL,
+    create_date      TIMESTAMP      NOT NULL,
+    last_update_date TIMESTAMP      NOT NULL,
+    CONSTRAINT uq_gift_certificates_name UNIQUE (name)
 );
-
-CREATE TRIGGER set_timestamp
-    BEFORE UPDATE
-    ON gift_certificates
-    FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_last_update_date();
 
 CREATE TABLE IF NOT EXISTS gift_certificates_tags
 (
-    id                  BIGSERIAL NOT NULL,
-    gift_certificate_id BIGSERIAL NOT NULL,
-    tag_id              BIGSERIAL NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (gift_certificate_id) REFERENCES gift_certificates (id)
-        ON DELETE CASCADE,
-    FOREIGN KEY (tag_id) REFERENCES tags (id)
-        ON DELETE CASCADE
+    gift_certificate_id BIGINT NOT NULL,
+    tag_id              BIGINT NOT NULL,
+    CONSTRAINT pk_gift_certificates_tags PRIMARY KEY (gift_certificate_id, tag_id),
+    CONSTRAINT fk_gift_certificates_tags_gift_certificate_id FOREIGN KEY (gift_certificate_id) REFERENCES gift_certificates (id),
+    CONSTRAINT fk_gift_certificates_tags_tag_id FOREIGN KEY (tag_id) REFERENCES tags (id)
 );
