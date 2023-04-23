a1f1d11396d8ef3579d9fe0f8131a50e5b370833
misha <mikhail.kutovenko@gmail.com>
2023-04-24 01:08:40 +0300

    Refactor imports

--- src/test/java/ru/clevertec/ecl/integration/web/controller/GiftCertificateControllerTest.java
+++ src/test/java/ru/clevertec/ecl/integration/web/controller/GiftCertificateControllerTest.java
@@ -12,7 +12,10 @@ import ru.clevertec.ecl.web.model.GiftCertificateModel;
 
 import java.math.BigDecimal;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
