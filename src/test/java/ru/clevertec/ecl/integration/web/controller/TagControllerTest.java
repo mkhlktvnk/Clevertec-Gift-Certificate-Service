8638c3c6889d1617a909961ffba8ce144fd60cd9
misha <mikhail.kutovenko@gmail.com>
2023-04-24 01:08:21 +0300

    Refactor

--- src/test/java/ru/clevertec/ecl/integration/web/controller/TagControllerTest.java
+++ src/test/java/ru/clevertec/ecl/integration/web/controller/TagControllerTest.java
@@ -1,6 +1,5 @@
 package ru.clevertec.ecl.integration.web.controller;
 
-import builder.impl.TagTestDataBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.clevertec.ecl.domain.entity.Tag;
 import ru.clevertec.ecl.integration.BaseIntegrationTest;
 import ru.clevertec.ecl.web.model.TagModel;
 
@@ -47,38 +45,33 @@ class TagControllerTest extends BaseIntegrationTest {
     @Test
     @SneakyThrows
     void findByIdShouldReturnCorrectTagAndOkStatus() {
-        Tag expectedTag = TagTestDataBuilder.aTag()
-                .withId(1L)
-                .withName("food")
-                .build();
-
         mockMvc.perform(get("/api/v0/tags/" + TAG_ID))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.id").value(expectedTag.getId()))
-                .andExpect(jsonPath("$.name").value(expectedTag.getName()));
+                .andExpect(jsonPath("$.id").value(TAG_ID));
     }
 
     @Test
     @SneakyThrows
     void findUserMostPopularTagWithTheHighestCostOfAllOrdersShouldReturnCorrectTagAndOkStatus() {
-        Tag expectedTag = TagTestDataBuilder.aTag()
-                .withId(2L)
-                .withName("travel")
+        TagModel expectedModel = TagModel.builder()
+                .id(2L)
+                .name("travel")
                 .build();
 
         mockMvc.perform(get("/api/v0/tags/popular?userId=" + USER_ID))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.id").value(expectedTag.getId()))
-                .andExpect(jsonPath("$.name").value(expectedTag.getName()));
+                .andExpect(jsonPath("$.id").value(expectedModel.getId()))
+                .andExpect(jsonPath("$.name").value(expectedModel.getName()));
     }
 
     @Test
     @SneakyThrows
     void saveShouldCreateNewTagAndReturnCorrectTagModelAndCreatedStatus() {
-        TagModel expectedModel = new TagModel();
-        expectedModel.setName("health");
+        TagModel expectedModel = TagModel.builder()
+                .name("health")
+                .build();
 
         String requestBody = objectMapper.writeValueAsString(expectedModel);
 
@@ -94,8 +87,9 @@ class TagControllerTest extends BaseIntegrationTest {
     @Test
     @SneakyThrows
     void updateByIdShouldUpdateTagByIdAndReturnNoContentStatus() {
-        TagModel updateTag = new TagModel();
-        updateTag.setName("books");
+        TagModel updateTag = TagModel.builder()
+                .name("books")
+                .build();
 
         String requestBody = objectMapper.writeValueAsString(updateTag);
 
